02d65c352c6b9b7f50164da0a2dd9dcc4b97a254
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2023-01-17 18:32:03 +0100

    Finish populateFamilyMethod

--- Genealogie/Program.cs
+++ Genealogie/Program.cs
@@ -1,19 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Genealogie;
 
+string[] genres = new string[] { "fils", "fille" };
 
 /*
- * ------------------------------Papi--------------------------------
- * ------Papa---------        ----Tonton-----      ----Tatie------
- * Frere soeur     moi       Cousin 1 Cousin 2     Cousin 3 Cousin4
- *       neuveu   
+ * ------------------------------Père--------------------------------1
+ * ------Fils---------        ----Fils-----      ----Fils------2
+ * Petit Fils soeur     moi       Cousin 1 Cousin 2     Cousin 3 Cousin43
+ *       Arrière petit fils   4
  * */
 
 
 
 
 
-printChild(populateFamily(4), "  ");
+printChild(populateFamily(5), "  ");
 
 /*
  * 
@@ -21,14 +22,14 @@ printChild(populateFamily(4), "  ");
  * Chaque membre aura un nombre aléatoire d'enfant
  * 
  * */
-Humain populateFamily(int nbGenerationSousParentActuel, Humain parentActuel=null)
+Humain populateFamily(int nbGenerationSousParentActuel, Humain parentActuel=null, int cptGeneration= 0)
 {
     //JE dois créer papi si parentActuel est null
     if(parentActuel == null)
     {
-        parentActuel = new Humain("Papi", new List<Humain>());
+        parentActuel = new Humain("Père", new List<Humain>());
     }
-
+    cptGeneration++;
     if (nbGenerationSousParentActuel != 0)
     {
         //Je génère un nombre aléatoire
@@ -39,9 +40,37 @@ Humain populateFamily(int nbGenerationSousParentActuel, Humain parentActuel=null
         //Je dois ajouter les enfants à papi
         for (int i = 0; i < nbChild; i++)
         {
-            var child = new Humain("Fils", new List<Humain>());
+            //J'ai fils
+            //Au hasard que ce soit fils ou fille
+            string nameChild = genres[rnd.Next(2)];
+
+            if (cptGeneration >= 2)
+            {
+                if (nameChild == genres[1])
+                {
+                    nameChild = "Petite " + nameChild;
+
+                }
+                else
+                {
+                    nameChild = "Petit " + nameChild;
+                }
+            }
+            
+            if (cptGeneration >= 3)
+            {
+                //je veux ajouter 'Arrière' cptArriere fois
+                int cptArriere = cptGeneration - 2;
+
+                for (int j = 0; j < cptArriere; j++)
+                {
+                    nameChild = "Arrière " + nameChild;
+                }
+            }
+
+            var child = new Humain(nameChild, new List<Humain>());
             //J'ajoute des enfants à chacuns de mes enfants
-            populateFamily(nbGenerationSousParentActuel - 1, child);
+            populateFamily(nbGenerationSousParentActuel - 1, child, cptGeneration);
             parentActuel.Enfants.Add(child);
         }
 
@@ -55,6 +84,6 @@ void printChild(Humain man, string indent)
     Console.WriteLine(indent + man.Nom);
     foreach (var child in man.Enfants)
     {
-        printChild(child, indent+ "  ");
+        printChild(child, indent+ "            ");
     }
 }
